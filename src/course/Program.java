566296379e0aa719c74e52e6f4c0e540b3a5d156
Soraya Brasil <sorayabrasil.gweb8@gmail.com>
2020-03-13 09:47:33 -0300

    Projeto criado

---
+++ src/course/Program.java
@@ -0,0 +1,33 @@
+package course;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		double n1 = 20;
+		double n2 = 50; 
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		
+		n1+= 2;
+		n2*= n1;
+		n1/= n2;
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n1);
+				
+		
+		
+	
+		
+		
+		
+		
+	}
+	
+
+}
